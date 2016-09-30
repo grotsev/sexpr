5492311408415a62f8bd5a17ad98519ad1b9578e
Denis Grotsev <dgrotsev@greet-go.com>
2016-09-30 13:19:24 +0300

    add sexpr example

--- src/kz/greetgo/sexpr/LispParserDemo.java
+++ src/kz/greetgo/sexpr/LispParserDemo.java
@@ -7,6 +7,7 @@ import kz.greetgo.sexpr.LispTokenizer;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +26,8 @@ public class LispParserDemo
       LispParser.Expr sexpr = parser.parseExpr();
       Object result = RtdmInterpreter.eval(sexpr, scope);
       System.out.println(result);
+
+      System.out.println(example());
     }
     catch (ParseException e1)
     {
@@ -32,4 +35,23 @@ public class LispParserDemo
       e1.printStackTrace();
     }
   }
+
+  public static LispParser.Expr example() {
+    return l(a("switch"), l( l(a("="), a("name"), s("Peter")), s("SMS to client") ));
+  }
+
+  public static ExprList l(LispParser.Expr... exprs) {
+    ExprList expr = new ExprList();
+    expr.addAll(Arrays.asList(exprs));
+    return expr;
+  }
+
+  public static Atom a(String name) {
+    return new Atom(name);
+  }
+
+  public static StringAtom s(String text) {
+    return new StringAtom(text);
+  }
+
 }
